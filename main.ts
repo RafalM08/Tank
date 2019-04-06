2ce3fead943ef363641df513049b3db56eb391f5
RafalM08 <48381406+RafalM08@users.noreply.github.com>
2019-04-06 12:00:23 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+ function lewo() {
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 0)
+}
+
+ function prawwo() {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+ }
+ function prosto() {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, 0)
+ }

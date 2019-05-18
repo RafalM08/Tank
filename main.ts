b0b7884d54c87e124425029b729b7566b2e4f79d
RafalM08 <48381406+RafalM08@users.noreply.github.com>
2019-05-18 11:46:40 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -11,7 +11,7 @@
 /**
  *This is Tank Motors:motor user motor and steering control function.
  */
-//% weight=10 color=#094400 icon="\uf013" block="Tank"
+//% weight=10 color=#073800 icon="f46f" block="Tank"
 
 
 namespace Tank {
@@ -22,9 +22,8 @@ namespace Tank {
     //% weight=100
     //% blockId=motor_control block="M1|%speed1|M2|%speed2"
     //% speed1.min=0 speed2.max=255
-    function motors(speed1: number, speed2: number) {
+    export function motors(speed1: number, speed2: number) {
         motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed1)
         motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed2)
     }
-
 }
